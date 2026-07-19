e6ad428cde277fad8fb692d8929682c0b68fd0ce
Amith Krishnan <amithkrishna29z@gmail.com>
2026-07-19 09:13:41 +0530

    Move CompositeTest public class to top of file

--- structural/composite/graphic/CompositeTest.java
+++ structural/composite/graphic/CompositeTest.java
@@ -3,6 +3,32 @@ package structural.composite.graphic;
 import java.util.ArrayList;
 import java.util.List;
 
+
+public class CompositeTest {
+    public static void main(String[] args) {
+        Ellipse ellipse1 = new Ellipse();
+        Ellipse ellipse2 = new Ellipse();
+        Graphic arc = new Arc();
+        Graphic rect = new Rectangle();
+
+        CompositeGraphic graphic = new CompositeGraphic();
+        CompositeGraphic graphic1 = new CompositeGraphic();
+        CompositeGraphic graphic2 = new CompositeGraphic();
+
+        graphic1.add(ellipse1);
+        graphic1.add(ellipse2);
+        graphic1.add(arc);
+
+        graphic2.add(rect);
+
+        graphic.add(graphic1);
+        graphic.add(graphic2);
+
+        graphic.print();
+    }
+
+}
+
 interface Graphic {
     public void print();
 }
@@ -43,27 +69,4 @@ class Arc extends Ellipse {
     }
 }
 
-public class CompositeTest {
-    public static void main(String[] args) {
-        Ellipse ellipse1 = new Ellipse();
-        Ellipse ellipse2 = new Ellipse();
-        Graphic arc = new Arc();
-        Graphic rect = new Rectangle();
 
-        CompositeGraphic graphic = new CompositeGraphic();
-        CompositeGraphic graphic1 = new CompositeGraphic();
-        CompositeGraphic graphic2 = new CompositeGraphic();
-
-        graphic1.add(ellipse1);
-        graphic1.add(ellipse2);
-        graphic1.add(arc);
-
-        graphic2.add(rect);
-
-        graphic.add(graphic1);
-        graphic.add(graphic2);
-
-        graphic.print();
-    }
-
-}
